232386a44a40d9ed265d3e9dc027c9966541a32b
DESKTOP-56IENH5\coderluan <zhoubo616819598@sina.com>
2017-04-28 14:41:26 +0800

    Update files.

--- Laya/linkgame.js
+++ Laya/linkgame.js
@@ -1,173 +1,173 @@
-// 设定全局参数
-var g_game_title = "五十音图对对碰";
-var g_stat_width = 800;
-var g_stat_heiht = 600;
-var g_list_A = ["あ", "い", "う", "え", "お", "か", "き", "く", "け", "こ", "さ", "し", "す", "せ", "そ", "た", "ち", "つ", "て", "と", "な", "に", "ぬ", "ね", "の", "は", "ひ", "ふ", "へ", "ほ", "ま", "み", "む", "め", "も", "や", "ゆ", "よ", "ら", "り", "る", "れ", "ろ", "わ", "を", "ん"];
-var g_list_B = ["ア", "イ", "ウ", "エ", "オ", "カ", "キ", "ク", "ケ", "コ", "サ", "シ", "ス", "セ", "ソ", "タ", "チ", "ツ", "テ", "ト", "ナ", "ニ", "ヌ", "ネ", "ノ", "ハ", "ヒ", "フ", "ヘ", "ホ", "マ", "ミ", "ム", "メ", "モ", "ヤ", "ユ", "ヨ", "ラ", "リ", "ル", "レ", "ロ", "ワ", "ヲ", "ン"];
-
-var gameStatue = false;
-
-if (g_list_A.length != g_list_B.length) {
-	alert("[Error] 数据长度不匹配！！！");
+//全局
+var W = 800;//宽度
+var H = 600;//高度
+var S1 = [//平假名
+	"あ", "い", "う", "え", "お",
+	"か", "き", "く", "け", "こ",
+	"さ", "し", "す", "せ", "そ",
+	"た", "ち", "つ", "て", "と",
+	"な", "に", "ぬ", "ね", "の",
+	"は", "ひ", "ふ", "へ", "ほ",
+	"ま", "み", "む", "め", "も",
+	"や", "ゆ", "よ", "ら", "り",
+	"る", "れ", "ろ", "わ", "を",
+	"ん"];
+var S2 = [//片假名
+	"ア", "イ", "ウ", "エ", "オ",
+	"カ", "キ", "ク", "ケ", "コ",
+	"サ", "シ", "ス", "セ", "ソ",
+	"タ", "チ", "ツ", "テ", "ト",
+	"ナ", "ニ", "ヌ", "ネ", "ノ",
+	"ハ", "ヒ", "フ", "ヘ", "ホ",
+	"マ", "ミ", "ム", "メ", "モ",
+	"ヤ", "ユ", "ヨ", "ラ", "リ",
+	"ル", "レ", "ロ", "ワ", "ヲ",
+	"ン"];
+var CARD = [];//卡片图像
+var LABEL = [];//卡片文字
+var VALUE1 = -1;//第一次点击的id
+var VALUE2 = -1;//第二次点击的id
+var VALUEC = 0;//配对成功的次数
+var ST = false;//游戏状态
+var CR = 0;//计时
+var ER = 0;//错误计数
+var DA = "";//显示数据
+//引擎
+{
+	Laya.init(W, H);
+	Laya.stage.scaleMode = Laya.Stage.SCALE_SHOWALL;
+	Laya.stage.screenMode = Laya.Stage.SCREEN_HORIZONTAL;
+	Laya.stage.alignH = Laya.Stage.ALIGN_CENTER;
+	Laya.stage.alignV = Laya.Stage.ALIGN_MIDDLE;
 }
-
-// 随机下标顺序
-var g_list_C = [];
-for (var i = 0; i < g_list_A.length; i++) {
-	g_list_C[i] = i;
+//背景
+{
+	var bg = new Laya.Sprite();
+	bg.loadImage("linkgame_back.jpg");
+	bg.cacheAsBitmap = true;
+	Laya.stage.addChild(bg);
 }
-
-function GetRandomNum(Min, Max) {
-	var Range = Max - Min;
-	var Rand = Math.random();
-	return (Min + Math.round(Rand * Range));
+//标题
+{
+	var lb = new Laya.Label();
+	lb.text = "五十音图对对碰";
+	lb.font = "Arial";
+	lb.fontSize = 50;
+	lb.stroke = 2;
+	lb.pos(W / 2 - lb.width / 2, 40);
+	Laya.stage.addChild(lb);
 }
-
-function debugInfo(str) {
-	var txt  = new  laya.display.Text();
-	txt.text = "[Debug] " + str;
-	txt.fontSize = 10;
-	Laya.stage.addChild(txt);
+//版权信息
+{
+	var cp = new Laya.Label();
+	cp.text = "Copyright © 2016 程序员之乱";
+	cp.font = "Arial";
+	cp.fontSize = 20;
+	cp.pos(20, H - 35);
+	cp.on(Laya.Event.CLICK, this, function () {
+		window.location.href = "http://www.coderluan.com";
+	});
+	Laya.stage.addChild(cp);
 }
-
-// 设定环境
-Laya.init(g_stat_width, g_stat_heiht);
-//Laya.Stat.show(0, 0);
-
-// 设定舞台属性
-Laya.stage.scaleMode = Laya.Stage.SCALE_SHOWALL;
-Laya.stage.screenMode = Laya.Stage.SCREEN_HORIZONTAL;
-Laya.stage.alignH = Laya.Stage.ALIGN_CENTER;
-Laya.stage.alignV = Laya.Stage.ALIGN_MIDDLE; 
-
-// 设定背景
-var background = new Laya.Sprite();
-background.loadImage("linkgame_back.jpg");
-background.cacheAsBitmap = true;
-Laya.stage.addChild(background);
-
-// 设定标题
-var title = new laya.display.Text();
-title.text = g_game_title;
-title.font = "Arial";
-title.fontSize = 50;
-title.stroke = 2;
-title.strokeColor = "Black";
-title.pos(g_stat_width / 2 - title.width / 2, 40);
-Laya.stage.addChild(title);
-
-// 设定版权信息
-var copyright = new laya.display.Text();
-copyright.text = "Copyright © 2016 程序员之乱";
-copyright.font = "Arial";
-copyright.fontSize = 20;
-copyright.bold = true;
-copyright.pos(20, g_stat_heiht - 35);
-copyright.on(Laya.Event.CLICK, this, function() {
-	window.location.href = "http://www.coderluan.com";
-});
-Laya.stage.addChild(copyright);
-
-// 设定开始按钮与计时器
-var  txt  = new  laya.display.Text();
-txt.text = "点击此处开始游戏";
-txt.font = "Arial";
-txt.fontSize = 30;
-txt.bold = true;
-txt.pos(g_stat_width - txt.width - 40, g_stat_heiht - txt.height - 20);
-Laya.stage.addChild(txt);
-txt.on(Laya.Event.CLICK, this, function(e) {
-	switch (e.type) {
-		case Laya.Event.CLICK:
-			gameStart();
-			break;
-	}
-}); //注册鼠标点击
-
-var card_A = -1;
-var card_B = -1;
-var winCount = 0;
-
-var timeCount = 0;
-var errorCount = 0;
-
-var card = [];
-var button = [];
-
-// 设定卡片
-for (var i = 0; i < 8; i++) {
-	for (var j = 0; j < 3; j++) {
-		var index = 8 * j + i
-		card[index] = new Laya.Sprite();
-		Laya.stage.addChild(card[index]);
-		card[index].name = index;
-		card[index].graphics.drawRect(0, 0, 80, 120, null, "#000000", 2);
-		card[index].graphics.loadImage("linkgame_card.jpg");
-		card[index].size(80, 120);
-		card[index].pos(10 + 100 * i, 130 + 140 * j);
-		card[index].on(Laya.Event.CLICK, this, function(e) {
-			if (gameStatue == true) {
-				if ((card_A == -1) && (card_B == -1)) {
-					card_A = e.target.name;
-					button[card_A].color = "red";
-				} else if ((card_A != -1) && (card_B == -1)) {
-					card_B = e.target.name;
-					if ((g_list_A.indexOf(button[card_A].text) != -1) && (g_list_A.indexOf(button[card_A].text) == g_list_B.indexOf(button[card_B].text)) || (g_list_A.indexOf(button[card_A].text) == -1) && (g_list_A.indexOf(button[card_B].text) == g_list_B.indexOf(button[card_A].text))) {
-						button[card_A].color = "red";
-						button[card_B].color = "red";
-						winCount = winCount + 1;
-						if (winCount == 8 * 3 / 2) {
-							//游戏结束
-							Laya.timer.clear(this, gameCounter);
-							var score = 100 - timeCount - errorCount;
-							txt.text = "游戏结束 得分 : " + score;
-						}
-					} else {
-						button[card_A].color = "black";
-						button[card_B].color = "black";
-						errorCount += 1;
-					}
-				} else if ((card_A != -1) && (card_B != -1)) {
-					card_A = e.target.name;
-					card_B = -1;
-					button[card_A].color = "red";
-				}
-			}
-		});
-		button[index] = new laya.display.Text();
-		Laya.stage.addChild(button[index]);
-		button[index].fontSize = 50;
-		button[index].pos(5 + 100 * i + 40 - 50 / 2, 150 + 140 * j + 60 - 50 / 2);
+//卡片
+{
+	for (var i = 0; i < 8; i++) {
+		for (var j = 0; j < 3; j++) {
+			var id = 8 * j + i;
+			CARD[id] = new Laya.Image("linkgame_card.jpg");
+			CARD[id].id = id;
+			CARD[id].size(80, 120);
+			CARD[id].pos(10 + 100 * i, 130 + 140 * j);
+			CARD[id].on(Laya.Event.CLICK, this, check);
+			Laya.stage.addChild(CARD[id]);
+			LABEL[id] = new Laya.Label();
+			LABEL[id].id = id;
+			LABEL[id].text = "あ";
+			LABEL[id].fontSize = 50;
+			LABEL[id].pos(20 + 100 * i, 185 + 140 * j);
+			LABEL[id].on(Laya.Event.CLICK, this, check);
+			Laya.stage.addChild(LABEL[id]);
+		}
 	}
 }
-
-// 开始游戏
-function gameStart() {
-
-	if (!gameStatue) {
-		gameStatue = true;
-		//初始化随机下标数组
-		g_list_C.sort(function() {
+//开始按钮
+{
+	var lb = new Laya.Label();
+	lb.text = "点击此处开始游戏";
+	lb.font = "Arial";
+	lb.fontSize = 30;
+	lb.stroke = 2;
+	lb.pos(W - lb.width - 40, H - lb.height - 20);
+	Laya.stage.addChild(lb);
+	lb.on(Laya.Event.CLICK, this, function (e) {
+		switch (e.type) {
+			case Laya.Event.CLICK:
+				start();//开始游戏
+				break;
+		}
+	});
+	DA = lb;//引用
+}
+//开始
+function start() {
+	if (ST == false) {
+		var id = [];
+		var ts = [];
+		for (var i = 0; i < S1.length; i++) {
+			id[i] = i;
+		}
+		//随机取12对数据
+		id.sort(function () {
 			return 0.5 - Math.random()
 		})
-		var g_list_word = [];
-		for (var i = 0; i < 8 * 3 / 2; i++) {
-			g_list_word[i] = g_list_A[g_list_C[i]];
-			g_list_word[i + 8 * 3 / 2] = g_list_B[g_list_C[i]];
+		for (var i = 0; i < 12; i++) {
+			ts[i] = S1[id[i]];
+			ts[i + 12] = S2[id[i]];
 		}
-		g_list_word.sort(function() {
+		//打乱12对数据顺序
+		ts.sort(function () {
 			return 0.5 - Math.random()
 		})
 		for (var i = 0; i < 24; i++) {
-			button[i].text = g_list_word[i];
+			LABEL[i].text = ts[i];
 		}
-
-		//开始计时
-		Laya.timer.loop(1000, this, gameCounter);
+		//计时
+		Laya.timer.loop(1000, this, counter);
+		ST = true;
+	}
+}
+//判断
+function check(e) {
+	if (ST == true) {
+		if ((VALUE1 == -1) && (VALUE2 == -1)) {//第一次点击
+			VALUE1 = e.target.id;
+			LABEL[VALUE1].color = "red";
+		} else if ((VALUE1 != -1) && (VALUE2 == -1)) {//偶数次点击
+			VALUE2 = e.target.id;
+			if ((S1.indexOf(LABEL[VALUE1].text) != -1)
+				&& (S1.indexOf(LABEL[VALUE1].text) == S2.indexOf(LABEL[VALUE2].text))
+				|| (S1.indexOf(LABEL[VALUE1].text) == -1)
+				&& (S1.indexOf(LABEL[VALUE2].text) == S2.indexOf(LABEL[VALUE1].text))) {//正确
+				LABEL[VALUE1].color = LABEL[VALUE2].color = "red";
+				//游戏结束
+				if (++VALUEC == 12) {
+					Laya.timer.clear(this, counter);
+					DA.text = "游戏结束 得分 : " + (100 - CR - ER * 5);
+					ST = false;
+				}
+			} else {//错误
+				LABEL[VALUE1].color = LABEL[VALUE2].color = "black";
+				ER += 1;
+			}
+		} else if ((VALUE1 != -1) && (VALUE2 != -1)) {//奇数次点击
+			VALUE1 = e.target.id;
+			VALUE2 = -1;
+			LABEL[VALUE1].color = "red";
+		}
 	}
 }
 
-function gameCounter() {
-	txt.text  =  "用时 : " + timeCount + "  失误:" + errorCount;
-	timeCount += 1;
+function counter() {
+	DA.text = "用时 : " + CR + "  失误:" + ER;
+	CR += 1;
 }
